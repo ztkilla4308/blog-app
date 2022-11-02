1b148ab822daf30d17f9595140720970f3822ff6
ztkilla <ztkilla4308@gmail.com>
2022-11-02 17:28:59 +0300

    fixed nextjs bugs

--- pages/components/Header.jsx
+++ pages/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 function Header() {
     return (
-        <nav className='grid grid-cols-12 mx-auto'>
+        <nav className='grid grid-cols-12 mx-auto container'>
             <div className='col-span-8 col-start-3 py-4'>
                 <Link href='/'>
                     <h1 className='text-xl'>Home</h1>
